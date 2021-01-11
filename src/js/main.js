30093925e525c49334e19d2b3ef796f5d21b40c3
nSedrickm <nsedrick101@gmail.com>
2021-01-11 07:36:29 +0100

    changes *

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,16 @@ import 'bootstrap';
 
 import GLightbox from 'glightbox';
 
+import LocomotiveScroll from 'locomotive-scroll';
+
 const lightbox = GLightbox({
-    touchNavigation: true,
-    loop: true,
-    zoomable: true,
-    openEffect:"fade"
+  touchNavigation: true,
+  loop: true,
+  zoomable: true,
+  openEffect: "fade"
+});
+
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
 });
