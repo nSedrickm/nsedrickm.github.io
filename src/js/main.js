66907403ee89de71a79f9b80afda43dd0673c3f2
nSedrickm <nsedrick101@gmail.com>
2021-01-11 03:49:45 +0100

    add projects section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 // All the magic goes here
 
 import 'bootstrap';
+
+import GLightbox from 'glightbox';
+
+const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    zoomable: true,
+    openEffect:"fade"
+});
